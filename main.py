e4ced4830c6cea308216e529e4f4c71966a2cacb
litecj <litecj702@gmail.com>
2022-05-24 20:36:34 +0900

    [check] fastapi

--- main.py
+++ main.py
@@ -2,11 +2,9 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
